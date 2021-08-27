a25baa7ce0782f0e2113dfb8248a54985f0120e2
Kheyyon <parktwins1@gmail.com>
2021-08-27 11:41:45 -0400

    I added functional logic to Layout

--- meal-planner/client/src/layout/Layout.jsx
+++ meal-planner/client/src/layout/Layout.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default function Layout() {
+export default function Layout(props) {
   return (
-    <div>
-      
-    </div>
+    <header>
+    <h1>App Name</h1>
+    {currentUser ? (
+      <div>
+        <p>{currentUser.username}</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    ) : (
+      <Link to='/login'>Login/Register</Link>
+    )}
+    <hr />
+    {currentUser && (
+      <div>
+        <Link to='/Menus'>Menus</Link>
+        <Link to='/'>Home</Link>  
+      </div>
+    )}
+    {props.children}
+  </header>
   )
 }
