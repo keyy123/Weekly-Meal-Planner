a18ac241bd5a0cf89cd89479b117e83bd6aca5a2
Kheyyon <parktwins1@gmail.com>
2021-08-27 11:23:35 -0400

    I connected to all endpoints for recipes

---
+++ meal-planner/client/src/services/recipes.js
@@ -0,0 +1,26 @@
+import api from "./api-config";
+
+export const readRecipes = async () => {
+  const res = api.get('/menus/:menu_id/recipes')
+  return res.data
+}
+
+export const readRecipe = async (id) => {
+  const res = api.get(`/menus/:menu_id/recipes/${id}`)
+  return res.data
+}
+
+export const createRecipe = async (data) => {
+  const res = api.post('/menus/:menu_id/recipes', { recipe: data })
+  return res.data
+}
+
+export const updateRecipe = async (id, data) => {
+  const res = api.put(`/menus/:menu_id/recipes/${id}`, { recipe: data })
+  return res.data
+}
+
+export const deleteRecipe = async (id) => {
+  const res = api.delete(`/menus/:menu_id/recipes/${id}`)
+  return res.data
+}
