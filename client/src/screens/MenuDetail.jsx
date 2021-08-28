c7400448bcaff0b46fa5ef090973f71896664bab
Kheyyon <parktwins1@gmail.com>
2021-08-28 18:10:35 -0400

    I am getting the ground work for recipes and menu detail ready adding the functions, and hooks needed for the recipe screens to function

--- client/src/screens/MenuDetail.jsx
+++ client/src/screens/MenuDetail.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react';
 import { Switch, Route, useParams } from 'react-router-dom';
 import { readOneMenu } from '../services/menus';
 import { readRecipes, readRecipe, createRecipe, updateRecipe, deleteRecipe } from '../services/recipes';
+import Recipes from '../screens/Recipes'
+import RecipeDetail from '../screens/RecipeDetail'
+import RecipeEdit from '../screens/RecipeEdit'
+import CreateRecipe from '../screens/CreateRecipe'
 //import { addFlavorToFood } from '../services/menus'; Add custom route for recipes here later
 
 
@@ -9,6 +13,7 @@ import { readRecipes, readRecipe, createRecipe, updateRecipe, deleteRecipe } fro
 //This will be the main container for my recipes which is nested within Menu Details (/menus/${id}/recipes) I also have a custom route for create for recipes
 export default function MenuDetail() {
   const [menuItem, setMenuItem] = useState(null);
+  const [recipes,setRecipes] = useState([])
   const { id } = useParams();
   
   //const { flavors } = props;
@@ -21,6 +26,29 @@ export default function MenuDetail() {
     }
     fetchMenuItem();
   }, [id]);
+
+  const handleCreate = async (formData) => {
+    const menuData = await createMenu(formData);
+    setMenus((prevState) => [...prevState, menuData]);
+    history.push('/menus');
+  };
+  
+  
+const handleUpdate = async (id, formData) => {
+  const menuData = await updateMenu(id, formData);
+  setMenus((prevState) =>
+    prevState.map((menu) => {
+      return menu.id === Number(id) ? menuData : menu;
+    })
+  );
+  history.push('/menus');
+};
+
+  
+const handleDelete = async (id) => {
+  await destroyMenu(id);
+  setMenus((prevState) => prevState.filter((menu) => menu.id !== id));
+};
 
   // const handleChange = (e) => {
   //   const { value } = e.target;
@@ -40,6 +68,25 @@ export default function MenuDetail() {
       <h3>{menuItem?.name}</h3>
       <p>Kcal/d: {menuItem?.kcal}</p>
       <p>Duration: {menuItem?.start_date} to {menuItem?.end_date}</p>
+
+      <Switch>
+        <Route path="menus/:menu_id/recipes/:id">
+          <RecipeEdit recipes={recipes} handleUpdate={handleUpdate}/>
+        </Route>
+        <Route path="menus/:menu_id/recipes/">
+          <CreateRecipe handleCreate={handleCreate}/>
+        </Route>
+        <Route path="menus/:menu_id/recipes/:id">
+          <RecipeDetail recipes={recipes}/>
+        </Route>
+        <Route path="menus/:menu_id/recipes">
+          <Recipes recipes={recipes}
+            handleDelete={handleDelete}
+            currentUser={currentUser}
+          />
+          
+        </Route>
+      </Switch>
       
     </div>
   )
