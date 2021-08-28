b874702835aeee7e228e332601f9ffdb67c11741
Kheyyon <parktwins1@gmail.com>
2021-08-27 21:17:01 -0400

    I added logic to the main container

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
+import {getAllMenus, getOneMenu, createMenu, destroyMenu, updateMenu} from '../services/menus'
 
-export default function MainContainer() {
+export default function MainContainer(props) {
+  const [menus, setMenus] = useState([])
+  const {currentUser} = props
+  const history = useHistory()
+
+  useEffect(() => {
+    const fetchMenus = async () => {
+      const menuList = await getAllMenus();
+      setMenus(menuList)
+    }
+    fetchMenus()
+},[])
+
+const handleCreate = async (formData) => {
+  const menuData = await createMenu(formData);
+  setFoods((prevState) => [...prevState, menuData]);
+  history.push('/menus');
+};
+
+const handleUpdate = async (id, formData) => {
+  const menuData = await updateMenu(id, formData);
+  setMenus((prevState) =>
+    prevState.map((menu) => {
+      return menu.id === Number(id) ? menuData : food;
+    })
+  );
+  history.push('/menus');
+};
+
+const handleDelete = async (id) => {
+  await destroyMenu(id);
+  setMenus((prevState) => prevState.filter((menu) => menu.id !== id));
+};
+
+  
+  
+  
+  
   return (
     <div>
-      Main Container
+      <Switch>
+        <Route path='/menus/:id/edit'>
+          <MenuEdit menus={menus} handleUpdate={handleUpdate} />
+        </Route>
+        <Route path='/menus/new'>
+          <createMenu handleCreate={handleCreate} />
+        </Route>
+        <Route path='/menus/:id'>
+          <MenuDetail menus={menus} />
+        </Route>
+        <Route path='/menus'>
+          <Menus
+            menus={menus}
+            handleDelete={handleDelete}
+            currentUser={currentUser}
+          />
+        </Route>
+      </Switch>
     </div>
   )
 }
