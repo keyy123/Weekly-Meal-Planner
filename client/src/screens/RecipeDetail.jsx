f9f606d3d3aa541b1348010c36a46fa289ab3cc8
Kheyyon <parktwins1@gmail.com>
2021-08-28 18:52:23 -0400

    I worked on recipes.jsx and RecipeDetail.jsx so far so good

--- client/src/screens/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
+import { readRecipe } from "../services/recipes"
 
 export default function RecipeDetail() {
+  const [recipe, setRecipe] = useState(null)
+  const { id } = useParams()
+  
+  
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      const recipeData = await readRecipe(id);
+      setRecipe(recipeData)
+    }
+    fetchRecipe()
+  }, [id])
+  
+  
+
   return (
     <div>
-      Details
+      <h3>{recipe?.name}</h3>
+      <p>Kcal/serv: {recipe?.kcal}</p>
+      <p>Protein(g): {recipe?.pro}</p>
+      <p>Carbohydrates(g): {recipe?.carbs}</p>
+      <p>Fats(g): {recipe?.fat}</p>
     </div>
   )
 }
--- client/src/screens/Recipes.jsx
+++ client/src/screens/Recipes.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
-export default function Recipes() {
+export default function Recipes(props) {
+
+  const {recipes, handleDelete, currentUser} = props
+  
   return (
     <div>
-      Index
+    <h3>Recipes</h3>
+      {recipes.map((recipe) => {
+        <div key={recipe.id}>
+          <Link to={`/menus/:menu_id/recipes/${recipe.id}`}>
+            <p>{recipe.name}</p>
+          </Link>
+          {currentUser (
+            <div>
+              <Link to={`/menus/:menu_id/recipes/${recipe.id}/edit`}>
+                <button>Edit</button>
+              </Link>
+              <button onClick={()=> handleDelete(recipe.id)}>Delete</button>
+            </div>             
+          )}
+        </div>
+      })}
+      <Link to='/menus/:menu_id/recipes/new'>
+        <button>Create</button>
+      </Link>
     </div>
   )
 }
