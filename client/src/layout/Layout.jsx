fbb1c68228851841fedd0fbd89fc89e95723f88f
Kheyyon <parktwins1@gmail.com>
2021-08-30 13:02:27 -0400

    I have media queries but I want to make them functional

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -4,14 +4,20 @@ import { makeStyles, ThemeProvider } from '@material-ui/styles'
 import theme from '../Components/ui/Theme.js'
 import "./Layout.css" 
 import logo from "../Assets/logo.png"
-import { Tabs, Tab } from '@material-ui/core'
+import { Tabs, Tab, useMediaQuery,useTheme } from '@material-ui/core'
 import { useState, useEffect } from 'react'
 
 
 const useStyles = makeStyles({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "1em"
+    marginBottom: "2em",
+    [theme.breakpoints.down("md")]:{
+      marginBottom: "1.5em"
+    },
+    [theme.breakpoints.down("xs")]:{
+      marginBottom: "1.2em"
+    }
   },
   tabContainer: {
 marginLeft:"auto"
@@ -19,7 +25,19 @@ marginLeft:"auto"
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px"
+    marginLeft: "25px",
+    [theme.breakpoints.down("md")]:{
+      color: "purple",
+    }
+  },
+  logo: {
+    height: "15em",
+    // [theme.breakpoints.down("md")]: {
+    //   height: "10em"
+    // },
+    // [theme.breakpoints.down("xs")]: {
+    //   height: "8.5em"
+    // }
   }
 
     })
@@ -27,8 +45,8 @@ marginLeft:"auto"
 
 export default function Layout(props) {
   const {currentUser, handleLogout} = props
-  
-
+  const theme = useTheme();
+ const matches = useMediaQuery(theme.breakpoints.down("md"))
 
   const classes = useStyles()
 
@@ -56,55 +74,60 @@ export default function Layout(props) {
     }
 },[value])
 
+  const tabs = (
+    <>
+      {currentUser ? (
+              
+              <>
+              <p>{currentUser.username}</p>
+              <Tabs
+                      value={value}
+                      indicatorColor="secondary"
+                      onChange={handleChange}
+                      className={classes.tabContainer}
+                    >
+                        
+                        <Tab value={0} className={classes.tab} label="Logout" onClick={handleLogout} component={Link} to="/Logout"/>
+                      
+                        <Tab value={1} className={classes.tab} label="Menus" component={Link} to="/menus"/>
+                        
+                          <Tab value={2} className={classes.tab} label="Home" component={Link} to="/Home"/>
+                      
+                      </Tabs>
+            </>
+                  ) : (
+                    
+                      <Tabs
+                        value={value}
+                        indicatorColor="secondary"
+                        onChange={handleChange}
+                        className={classes.tabContainer}
+                      >
+                          
+                          <Tab value={3} className={classes.tab} label="Login" component={Link} to="/Login"/>
+                        
+                          <Tab value={4} className={classes.tab} label="Register" component={Link} to="/Register"/>
+                    
+                      </Tabs>
+                      
+                      
+                  )}
+          {/* {currentUser && (
+            <div>
+               
+                  </div>
+                )} */}
+      </>
+  )
+  
   return (
     <>
     <ThemeProvider theme={theme}>
-      <AppBar position="fixed" color="primary">
+      <AppBar position="fixed" className="AppBar" >
           <Toolbar disableGutters>
-            <img src={logo} alt="company logo"/>
+            <img className={classes.logo}src={logo} alt="company logo"/>
     {/* <h1 className="Title">Meal Prepper</h1> */}
-    {currentUser ? (
-              
-        <>
-        <p>{currentUser.username}</p>
-        <Tabs
-                value={value}
-                indicatorColor="secondary"
-                onChange={handleChange}
-                className={classes.tabContainer}
-              >
-                  
-                  <Tab value={0} className={classes.tab} label="Logout" onClick={handleLogout} component={Link} to="/Logout"/>
-                
-                  <Tab value={1} className={classes.tab} label="Menus" component={Link} to="/menus"/>
-                  
-                    <Tab value={2} className={classes.tab} label="Home" component={Link} to="/Home"/>
-                
-                </Tabs>
-      </>
-            ) : (
-              
-                <Tabs
-                  value={value}
-                  indicatorColor="secondary"
-                  onChange={handleChange}
-                  className={classes.tabContainer}
-                >
-                    
-                    <Tab value={3} className={classes.tab} label="Login" component={Link} to="/Login"/>
-                  
-                
-                    <Tab value={4} className={classes.tab} label="Register" component={Link} to="/Register"/>
-              
-                </Tabs>
-                
-                
-            )}
-    {/* {currentUser && (
-      <div>
-         
-            </div>
-          )} */}
+    {matches ? null : tabs }
           </Toolbar>
       </AppBar>
       </ThemeProvider>
