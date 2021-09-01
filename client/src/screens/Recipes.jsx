10ecac3889b661000bb029620c1e4811b0ebd3d8
Kheyyon <parktwins1@gmail.com>
2021-09-01 10:36:29 -0400

    I updated the look of the recipes screen

--- client/src/screens/Recipes.jsx
+++ client/src/screens/Recipes.jsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from "react-router-dom"
-
+import { Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 export default function Recipes(props) {
   const {id} = useParams()
   const {recipes, handleDelete } = props
@@ -9,17 +10,27 @@ export default function Recipes(props) {
     <h3>Recipes</h3>
       {recipes?.map((recipe) => (
         <div key={recipe?.id}>
-          <Link to={`/menus/${id}/recipes/${recipe.id}`}>
+            <Accordion>
+        <AccordionSummary
+          style={{wordSpacing:"2px"}}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+            >
+            <Link to={`/menus/${id}/recipes/${recipe.id}`}>
             <p>{recipe?.name}</p>
-          </Link>
-          
+          </Link>     
+        </AccordionSummary>
+            <AccordionDetails style={{ display: "flex", flexDirection: "column" }}>
             <div>
               <Link to={`/menus/${id}/recipes/${recipe.id}/edit`}>
                 <button>Edit</button>
               </Link>
               <button onClick={()=> handleDelete(recipe.id)}>Delete</button>
             </div>             
-        
+        </AccordionDetails>
+        </Accordion>
+           
         </div>
       ))}
       <Link to={`/menus/${id}/recipes/new`}>
